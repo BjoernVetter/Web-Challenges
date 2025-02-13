857db71370c41be76587bb1bd4705dab496110af
Bjoern Vetter <vetterbjoern@yahoo.de>
2025-02-13 14:54:09 +0100

    fertig commit

--- js_conditions_and_booleans/js-conditions-and-booleans_statements/js/index.js
+++ js_conditions_and_booleans/js-conditions-and-booleans_statements/js/index.js
@@ -3,24 +3,49 @@ console.clear();
 // Part 1: Password
 const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
-const receivedPassword = "password1234";
+const receivedPassword = "password123";
 
+if (receivedPassword === SUPER_SECRET_PASSWORD) {
+  console.log("open");
+} else {
+  console.log("closed");
+}
 // Part 2: Even / Odd
-const number = 6;
+const number = 4;
+if (number % 2 === 0) {
+  console.log("gerade Zahl");
+} else {
+  console.log("ungerade Zahl");
+}
 
 // Part 3: Hotdogs
 const numberOfHotdogs = 42;
+if (numberOfHotdogs < 5) {
+  console.log(numberOfHotdogs * 2);
+} else if (numberOfHotdogs < 100) {
+  console.log(numberOfHotdogs * 1.5);
+} else if (numberOfHotdogs < 1000000) {
+  console.log(numberOfHotdogs * 1);
+} else {
+  console.log(numberOfHotdogs * 0.1);
+}
 
 // Part 4: Daytime
-const currentHour = 12;
+const currentHour = 17;
 
-const statement = "";
-
+const statement = currentHour <= 17 ? "Muss noch lernen" : "Partytime";
 console.log(statement);
 
-// Part 5: Greeting
-const userName = "Archibald";
 
-const greeting = "Hello " + "//enter your code here" + "!";
+
+
+
+
+
+// Part 5: Greeting
+const userName = "Archibal";
+const coachName = "Archibal";
+
+const greeting = "Hello " + (userName === coachName ? "Coach" : userName) + "!";
 
 console.log(greeting);
