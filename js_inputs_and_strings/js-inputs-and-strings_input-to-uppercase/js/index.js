e8aaa843b504f6a55641dbaf8cb4b93995ffa1d0
Bjoern Vetter <vetterbjoern@yahoo.de>
2025-02-17 19:49:42 +0100

    initial commit

--- js_inputs_and_strings/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js_inputs_and_strings/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,18 +1,3 @@
-// console.clear();
-
-// const input = document.querySelector('[data-js="first-input"]');
-// const button = document.querySelector('[data-js="button-uppercase"]');
-
-
-
-
-// button.addEventListener('click', () => {
-// input.value = input.value.toUppercase();
-
-// });
-
-
-
 
 console.clear();
 
