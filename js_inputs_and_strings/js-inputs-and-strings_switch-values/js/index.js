d2f0cb15c2ecb7c0c774b86bf3e2ca567f38f933
Bjoern Vetter <vetterbjoern@yahoo.de>
2025-02-17 18:45:34 +0100

    fertig commit

--- js_inputs_and_strings/js-inputs-and-strings_switch-values/js/index.js
+++ js_inputs_and_strings/js-inputs-and-strings_switch-values/js/index.js
@@ -4,10 +4,25 @@ const firstInput = document.querySelector('[data-js="first-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
 
+const secondInput = document.querySelector('[data-js="second-input"]');
+const switchcaseButton = document.querySelector('[data-js="button-switchcase"]');
+
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
 });
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+});
+
+
+
+switchcaseButton.addEventListener("click", () => {
+  const firstvalue = firstInput.value;
+  firstInput.value = secondInput.value;
+  secondInput.value = firstvalue;
+
+
+
+
 });
