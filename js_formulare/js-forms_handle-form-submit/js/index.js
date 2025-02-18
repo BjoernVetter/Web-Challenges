8c7f83ab681b66e9fbc6a9df47b81be927f56fe1
Bjoern Vetter <vetterbjoern@yahoo.de>
2025-02-18 12:23:14 +0100

    fertig commit

--- js_formulare/js-forms_handle-form-submit/js/index.js
+++ js_formulare/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,15 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    const formElement = event.target;
+    
+    const formData = new FormData(formElement);
+    const data = Object.fromEntries(formData);
+    
+    console. log(data);
+    
+});
