cf7e63fac36225dcda7f038fe71b6b63521ae382
Bjoern Vetter <vetterbjoern@yahoo.de>
2025-02-28 17:04:33 +0100

    fertig commit

--- react/react-props_greeting/src/App.jsx
+++ react/react-props_greeting/src/App.jsx
@@ -1,3 +1,17 @@
+function Greeting({ name }) {
+const isCoach = name === "Björn"; 
+  return <h1>Hello, {isCoach ? "Coach" : name}! </h1>;
+}
+
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Greeting name="Björn" />
+      <Greeting name="Levi" />
+      <Greeting name="Natali" />
+      <Greeting name="Jan" />
+      <Greeting name="Tanja" />
+    
+    </div>
+  );
 }
