1a3072768f71972db3717ad5243e7116d47026e5
Bjoern Vetter <vetterbjoern@yahoo.de>
2025-03-01 17:31:48 +0100

    fertig commit

--- js-array-methoden/js-array-methods_filter/js/index.js
+++ js-array-methoden/js-array-methods_filter/js/index.js
@@ -24,14 +24,18 @@ const cards = [
   },
 ];
 
-const onlyCardWithIdTwo = null;
+const onlyCardWithIdTwo = cards.filter((card) => card.id === "2");
+console.log(onlyCardWithIdTwo);
 
-const allCardsWith3Tags = null;
+const allCardsWith3Tags = cards.filter((card) => card.tags.length ===3);
+console.log(allCardsWith3Tags);
 
-const allCardsThatAreNotBookmarked = null;
 
-const allCardsWithTagsHTMLOrJSThatAreBookmarked = null;
+const allCardsThatAreNotBookmarked = cards.filter((card)=> !card.isBookmarked);
+console.log(allCardsThatAreNotBookmarked)
 
+const allCardsWithTagsHTMLOrJSThatAreBookmarked = cards.filter((card)=> card.isBookmarked && (card.tags.includes("html"))|| card.tags.includes("js"));
+console.log(allCardsWithTagsHTMLOrJSThatAreBookmarked)
 export {
   onlyCardWithIdTwo,
   allCardsWith3Tags,
