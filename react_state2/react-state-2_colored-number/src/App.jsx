9bf345203310111ae0a8c969b7db973d945dbce6
Bjoern Vetter <vetterbjoern@yahoo.de>
2025-03-04 16:22:16 +0100

    fertig commit

--- react_state2/react-state-2_colored-number/src/App.jsx
+++ react_state2/react-state-2_colored-number/src/App.jsx
@@ -1,6 +1,22 @@
+// import { useState } from "react";
 import "./styles.css";
 import Counter from "./components/Counter";
+import ColoredNumber from "./components/ColoredNumber";
 
 export default function App() {
-  return <Counter />;
+
+
+
+
+    return <Counter />;
+  
+    // <button onClick={() => setCount(count - 1)}</button>
+    // <ColoredNumber value={count} />;
+    // <button onClick={() => setCount(count + 1)}</button>
+
+
 }
+
+
+
+
