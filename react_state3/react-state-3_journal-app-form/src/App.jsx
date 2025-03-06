fddf3eaf4979bd7899ccca278dd7da65e26945fb
Bjoern Vetter <vetterbjoern@yahoo.de>
2025-03-06 19:12:02 +0100

    fertig

--- react_state3/react-state-3_journal-app-form/src/App.jsx
+++ react_state3/react-state-3_journal-app-form/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import EntriesSection from "./components/EntriesSection";
 import EntryForm from "./components/EntryForm";
@@ -5,12 +6,16 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
+  const [entries, setEntries] = useState ([]);
+  function handleAddEntry (newEntry) {
+    setEntries([...entries, newEntry]);
+  }
   return (
     <div className="app">
       <Header />
       <main className="app__main">
-        <EntryForm />
-        <EntriesSection />
+        <EntryForm onAddEntry={handleAddEntry} />
+        <EntriesSection entries={entries} />
       </main>
       <Footer />
     </div>
