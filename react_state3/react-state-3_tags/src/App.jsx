4d7e6ad3273d3d165cfa6962f5402967586623b4
Bjoern Vetter <vetterbjoern@yahoo.de>
2025-03-06 15:55:43 +0100

    fertig

--- react_state3/react-state-3_tags/src/App.jsx
+++ react_state3/react-state-3_tags/src/App.jsx
@@ -4,11 +4,15 @@ import Form from "./components/Form";
 import List from "./components/List";
 
 export default function App() {
-  const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
+  // const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
+  const [tags, setTags] = useState([]);
+  function handleAddTag (newTag) {
+    setTags([...tags, newTag]);
+  }
 
   return (
     <main className="app">
-      <Form />
+      <Form onAddTag={handleAddTag} />
       <List tags={tags} />
     </main>
   );
